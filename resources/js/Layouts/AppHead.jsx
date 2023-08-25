a82a586963617426505b4a9e0d8479230ea424e3
kiyoshion <kiyoshion@gmail.com>
2023-08-26 04:03:07 +0900

    add env for twitter ogp

--- resources/js/Layouts/AppHead.jsx
+++ resources/js/Layouts/AppHead.jsx
@@ -15,9 +15,9 @@ export default function AppHead({ title = '', desc = '', image = '' }) {
       <meta head-key='ogSiteName' property='og:site_name' content={import.meta.env.VITE_APP_NAME} />
 
       {/* Twitter */}
-      <meta head-key='twitterSite' name='twitter:site' content='@YOUR_TWITTER' />
+      <meta head-key='twitterSite' name='twitter:site' content={import.meta.env.OGP_TWITTER_SITE} />
       <meta head-key='twitterCard' name='twitter:card' content='summary_large_image' />
-      <meta head-key='twitterDomain' name='twitter:domain' content='YOUR_DOMAIN' />
+      <meta head-key='twitterDomain' name='twitter:domain' content={import.meta.env.OGP_TWITTER_DOMAIN} />
       <meta head-key='twitterTitle' name='twitter:title' content={title} />
       <meta head-key='twitterImage' name='twitter:image' content={image} />
       <meta head-key='twitterDescription' name='twitter:description' content={desc} />
